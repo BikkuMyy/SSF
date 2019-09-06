29cccb0c0f5037d9e30ee678d01adb42e6466838
Mari Trtnik <maripartan@gmail.com>
2019-09-06 16:13:51 +0300

    deploy

---
+++ precache-manifest.77cc95c7414fe03eef9aaf7760991ffc.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce2b8608515fec6b3270",
+    "url": "/SSF/css/app.14248d9d.css"
+  },
+  {
+    "revision": "ce2b8608515fec6b3270",
+    "url": "/SSF/js/app.88993150.js"
+  },
+  {
+    "revision": "14dbf65ee71cf5006cfc",
+    "url": "/SSF/css/chunk-vendors.902995e8.css"
+  },
+  {
+    "revision": "14dbf65ee71cf5006cfc",
+    "url": "/SSF/js/chunk-vendors.a4cab55d.js"
+  },
+  {
+    "revision": "1d92f48020b22f9c7be5fd9a084fc66d",
+    "url": "/SSF/img/gurula.1d92f480.jpg"
+  },
+  {
+    "revision": "f4aa25fed4b8450c85ea1de73715c3b0",
+    "url": "/SSF/img/egg.f4aa25fe.png"
+  },
+  {
+    "revision": "7d3e63d7f144e5cb60406e21e67001af",
+    "url": "/SSF/img/silent.7d3e63d7.png"
+  },
+  {
+    "revision": "497cb63053136a01c9d0fc4210866235",
+    "url": "/SSF/img/python.497cb630.png"
+  },
+  {
+    "revision": "e7c18ac645719373f2316051cba798d2",
+    "url": "/SSF/img/maths.e7c18ac6.png"
+  },
+  {
+    "revision": "0c8b67ec3a9f135f4af487e127b18b5b",
+    "url": "/SSF/img/survomo.0c8b67ec.png"
+  },
+  {
+    "revision": "787653f6ff7f570be3c4fa1f16c5d149",
+    "url": "/SSF/img/g103e.787653f6.png"
+  },
+  {
+    "revision": "7e363941a43cce3c633c97b51553fa51",
+    "url": "/SSF/img/library.7e363941.png"
+  },
+  {
+    "revision": "b145da787f77fe8d35d39078d770d3eb",
+    "url": "/SSF/img/quiet.b145da78.png"
+  },
+  {
+    "revision": "37ab4f6c23c76909ea315c8f17165b4a",
+    "url": "/SSF/img/suppa.37ab4f6c.png"
+  },
+  {
+    "revision": "bb56265936d3bf4da5f4deb9de00d9be",
+    "url": "/SSF/img/b221.bb562659.png"
+  },
+  {
+    "revision": "2739da8e7ae77bf3da1af6a9515ec66a",
+    "url": "/SSF/img/q.2739da8e.png"
+  },
+  {
+    "revision": "6af25d9ca58ae43b63b48217a78c339f",
+    "url": "/SSF/img/ratkomo.6af25d9c.png"
+  },
+  {
+    "revision": "e925b2e8cca5aacffb3ee410f0f7e687",
+    "url": "/SSF/img/octave.e925b2e8.png"
+  },
+  {
+    "revision": "cbd5911d7458743d1f81c0299231b92c",
+    "url": "/SSF/img/idris.cbd5911d.png"
+  },
+  {
+    "revision": "ba2b1f77b6a17432cae878e7840ea619",
+    "url": "/SSF/img/go.ba2b1f77.png"
+  },
+  {
+    "revision": "3a7f0430cf9e4026555177fb206a062a",
+    "url": "/SSF/img/modula.3a7f0430.png"
+  },
+  {
+    "revision": "643f1cce559cc98da45ef244e1b43e07",
+    "url": "/SSF/img/G108a.643f1cce.png"
+  },
+  {
+    "revision": "989504f7de2cd482aae7653e09bbfbdf",
+    "url": "/SSF/img/G103g.989504f7.png"
+  },
+  {
+    "revision": "6f936c18f596034de7ecab5fb7c781a0",
+    "url": "/SSF/img/haskell.6f936c18.png"
+  },
+  {
+    "revision": "ef8dd06d2d62063acc5e821070f216eb",
+    "url": "/SSF/img/G103c.ef8dd06d.png"
+  },
+  {
+    "revision": "4981393e46839a33ef9ec43695732ce4",
+    "url": "/SSF/img/lisp.4981393e.png"
+  },
+  {
+    "revision": "6986dabb7d1c0aef1fdf4348e43483cf",
+    "url": "/SSF/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/SSF/robots.txt"
+  }
+];
